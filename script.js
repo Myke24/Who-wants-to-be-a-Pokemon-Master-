3d315d4e4f563cdb800c66ed4867feb0feb4e071
Mykiel-Horn <mykiel.d.horn24@gmail.com>
2021-02-24 17:31:27 -0600

    main layout finished, working on losing and winning layouts

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const user = {
 	earnings: 0,
 	currentLevel: 0,
 };
+document.querySelector('#modal').style.display = 'none';
 let scoreArea = document.querySelector('#currentNpotentialScoreArea');
 let questionNAnswerArea = document.querySelector('#questionNAnswerArea');
 let scoringSection = document.querySelector('#scoringSection');
@@ -22,6 +23,7 @@ scoringMeter.children[
 question.innerText = 'What is the name of this Pokemon?';
 const pokemonSprite = document.querySelector('#pokemonSprite');
 let answers = document.querySelectorAll('.answer');
+const startOverBtn = document.querySelector('#startOverBtn');
 
 const startOver = async () => {
 	user.earnings = 0;
@@ -39,17 +41,31 @@ const startOver = async () => {
 	});
 };
 
+//--------------------Winning Algor-----------------------------
+function winIt() {
+	console.log('Congrads, you WON!... Nothing!');
+	document.querySelector('#modal').style.display = 'flex';
+}
+
 //--------------------Losing Algor----------------------------------
 
 let losses;
 
 function loseIt() {
 	let takeAwayFactor = () => {
-		if (Number(potentialEarnings.innerText) >= 250000) {
+		if (Number(potentialEarnings.innerText) >= 100000) {
 			return 10000;
-		} else if (Number(potentialEarnings.innerText) >= 1000) {
+		} else if (
+			Number(potentialEarnings.innerText) < 100000 &&
+			Number(potentialEarnings.innerText) >= 10000
+		) {
+			return 1000;
+		} else if (
+			Number(potentialEarnings.innerText) <= 10000 &&
+			Number(potentialEarnings.innerText) >= 5000
+		) {
 			return 100;
-		} else if (Number(potentialEarnings.innerText < 900)) {
+		} else if (Number(potentialEarnings.innerText < 1000)) {
 			return 25;
 		} else {
 			return 10;
@@ -57,13 +73,15 @@ function loseIt() {
 	};
 	potentialEarnings.innerText =
 		Number(potentialEarnings.innerText) - takeAwayFactor();
-	if (Number(potentialEarnings.innerText) <= 0) {
+	if (Number(potentialEarnings.innerText) < 0) {
 		clearInterval(losses);
 		setTimeout(() => {
 			document.body.innerHTML = `
 			<h1>Sorry you are not ready to be a Pokemon Master</h1>
 			<img src="https://media1.tenor.com/images/01b11fc630fccfe82dad009ea1e25c28/tenor.gif?itemid=16743813"/>
-			<button onclick="startOver()">Play Again?</button>`;
+			<button id="startOverBtn" onclick="startOver()">Play Again</button>
+			`;
+			// document.querySelector('#startOverBtn').style.display = 'block';
 		}, 1000);
 	}
 }
@@ -136,11 +154,14 @@ async function getPokemonData() {
 			});
 	}
 
-	answers[rand4[0]].innerText = wrongAnswers[0];
-	answers[rand4[1]].innerText = wrongAnswers[1];
-	answers[rand4[2]].innerText = wrongAnswers[2];
-	answers[rand4[3]].innerText = answer;
-	startTimer();
+	const createWrongAnswers = async () => {
+		answers[rand4[0]].innerText = wrongAnswers[0];
+		answers[rand4[1]].innerText = wrongAnswers[1];
+		answers[rand4[2]].innerText = wrongAnswers[2];
+		answers[rand4[3]].innerText = answer;
+		// startTimer();
+	};
+	await createWrongAnswers();
 }
 
 /* ------------------------------ What Pokemon is this? --------------------------------*/
@@ -163,12 +184,12 @@ const question1 = async () => {
 		if (selectedAnswer !== undefined) {
 			if (true) {
 				stopTimer();
-				console.log('Stopped timer');
 			}
 			if (selectedAnswer === answer) {
 				//This is where winning function should go---------------<-----------<------<
 				if (user.currentLevel === 9) {
-					console.log('Congraduates, you WON!... Nothing!');
+					stopTimer();
+					winIt();
 				}
 				user.currentLevel++;
 				potentialEarnings.innerText =
@@ -186,16 +207,23 @@ const question1 = async () => {
 						scoringMeter.children.length - user.currentLevel
 					].style.borderRadius = '10%';
 				}
-				scoringMeter.children[
-					scoringMeter.children.length - user.currentLevel - 1
-				].style.border = 'dotted lime';
-				question1();
+				console.log(scoringMeter.children.length - user.currentLevel - 1);
+				if (scoringMeter.children.length - user.currentLevel - 1 !== -1) {
+					scoringMeter.children[
+						scoringMeter.children.length - user.currentLevel - 1
+					].style.border = 'dotted lime';
+					question1();
+				}
 			} else {
 				stopTimer();
 				loseEarnings();
 			}
 		} else {
 			document.querySelector('#helper').style.display = 'block';
+			setTimeout(helperClear, 5000);
+			function helperClear() {
+				document.querySelector('#helper').style.display = 'none';
+			}
 		}
 	};
 };
